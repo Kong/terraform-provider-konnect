1940dcf38646a07b5b827e53415396965085e29a
Michael Heap <m@michaelheap.com>
2022-10-13 14:44:58 +0100

    Add OpenAPI Workflow (#11)
    Co-authored-by: Nicholas Anderson <iamnande@users.noreply.github.com>

---
+++ bin/preview.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+mkdir -p dist
+
+DOCS=$1
+
+if [[ -z "$DOCS" ]]; then
+  DOCS=$(find definitions -name 'openapi.yaml')
+fi
+
+rm -f dist/index.html
+echo "<ul>" >> dist/index.html
+for i in $DOCS; do
+  API=$(echo $i | sed 's#definitions/##')
+  mkdir -p dist/$(echo $API | sed 's#/openapi.yaml##')
+  ./node_modules/.bin/redoc-cli build $i -o dist/$API.html
+  echo "<li><a href='/$API.html'>$API</a></li>" >> dist/index.html
+done
+echo "</ul>" >> dist/index.html
