dd4d4645668b4c5c2106c688a9245b3a2a2f4004
Michael Heap <m@michaelheap.com>
2024-02-01 06:15:28 +0000

    Strip leading /mesh/ path from correct file (#482)

--- tools/process-mesh.sh
+++ tools/process-mesh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-MESH_SPEC="./src/konnect/definitions/mesh-control-planes/v0/openapi.yaml"
+MESH_SPEC="./computed/konnect/mesh-control-planes/v0/openapi.yaml"
 MESH=$(npx oas-toolkit rewrite-path --oldPath '^/mesh/' --newPath '/' $MESH_SPEC)
 echo "$MESH" > $MESH_SPEC
