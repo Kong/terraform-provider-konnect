34f075ea19cbc26da16067632d7a3286028a8f7a
Michael Heap <m@michaelheap.com>
2024-04-25 09:29:54 +0100

    Add new version of generated Control Planes Config API (#616)

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Merge the generated and hand maintained OAS files for Control Planes Config
+FILES=(
+  "src/konnect/definitions/control-planes-config/v2/generated.yaml"
+  "src/konnect/definitions/control-planes-config/v2/konnect.yaml"
+)
+
+./node_modules/.bin/oas-toolkit check-conflicts ${FILES[@]} --ignorePrefix components.securitySchemes --ignorePrefix components.examples
+./node_modules/.bin/oas-toolkit merge ${FILES[@]} > src/konnect/definitions/control-planes-config/v2/openapi.yaml
+
+# Build all services
 ./tools/delete-build.sh $1
 ./tools/merge-oas.sh $1
 node ./tools/generate-sdk-openapi-files.js $1
