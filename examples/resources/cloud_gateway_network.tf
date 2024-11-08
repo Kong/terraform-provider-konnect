9a69a54518afcedfc0ba49632219889e9cc310bb
Michael Heap <m@michaelheap.com>
2024-11-08 12:04:34 +0000

    Remove firewall+ddos_protection from CGW network examples

--- examples/resources/cloud_gateway_network.tf
+++ examples/resources/cloud_gateway_network.tf
@@ -7,14 +7,7 @@ resource "konnect_cloud_gateway_network" "my_cloudgatewaynetwork" {
     "euw1-az3"
   ]
 
-  firewall = {
-    allowed_cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-  }
-
   cidr_block      = "192.168.0.0/16"
-  ddos_protection = false
 
   cloud_gateway_provider_account_id = data.konnect_cloud_gateway_provider_account_list.my_cloudgatewayprovideraccountlist.data[0].id
 }
