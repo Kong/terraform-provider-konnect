2680dc8ac7c04d0d86a04dd2e27f2a3dabbb2eda
Michael Heap <m@michaelheap.com>
2024-11-11 11:40:00 +0000

    Update Portal documentation

--- examples/scenarios/publish-api-product.tf
+++ examples/scenarios/publish-api-product.tf
@@ -40,11 +40,12 @@ resource "konnect_gateway_route" "anything" {
   }
 }
 
-# Fetch the list of portals so that we can refer
-# to the Portal ID for publishing
-data "konnect_portal_list" "my_portallist" {
-  page_number = 1
-  page_size   = 1
+resource "konnect_portal" "my_portal" {
+  name                      = "My New Portal"
+  auto_approve_applications = false
+  auto_approve_developers   = false
+  is_public                 = false
+  rbac_enabled              = false
 }
 
 # API Product configuration
@@ -56,7 +57,7 @@ resource "konnect_api_product" "httpbin" {
   # Konnect only supports a single portal at the moment
   # so we can rely on the first portal in the list
   portal_ids = [
-    data.konnect_portal_list.my_portallist.data[0].id
+    konnect_portal.my_portal.id
   ]
 }
 
@@ -98,13 +99,12 @@ resource "konnect_portal_product_version" "my_portalproductversion" {
   deprecated                       = false
   publish_status                   = "published"
 
-  portal_id          = data.konnect_portal_list.my_portallist.data[0].id
+  portal_id          = konnect_portal.my_portal.id
   product_version_id = konnect_api_product_version.httpbin_v1.id
   auth_strategy_ids = [
     konnect_application_auth_strategy.my_applicationauthstrategy.id
   ]
 }
-
 
 resource "konnect_api_product_document" "my_apiproductdocument" {
   title          = "How to create a document in Konnect DocumentHub"
