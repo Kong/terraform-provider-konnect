a91e660531e219000c42d27223f333e88b658aef
Michael Heap <m@michaelheap.com>
2024-11-25 16:23:54 +0000

    Konnect API server list tooling (#827)
    * Add Middle-East region to Konnect servers block

--- tools/process-computed.js
+++ tools/process-computed.js
@@ -20,6 +20,7 @@ async function main() {
       console.log(`Processing ${f}`);
       let oas = yaml.load(await fs.readFile(f));
       oas = addUnstablePublicDescription(oas);
+      oas = await setServersBlock(oas, f);
       await fs.writeFile(f, yaml.dump(oas));
     }
   }
@@ -45,6 +46,35 @@ ${description}`.trim();
   });
 }
 
+async function setServersBlock(doc, file) {
+  console.log(file);
+  // We only want to process Konnect files
+  if (!file.includes("computed/konnect")) {
+    return doc;
+  }
+
+  // Make sure global servers block is set only for global endpoints
+  if (doc.servers.length == 1 && doc.servers[0].url.includes("global.api.konghq.com")) {
+    return doc;
+  }
+
+  // Extract the path from the current doc
+  const url = doc.servers[0].url;
+  const path = url.split(".konghq.com")[1];
+
+  const konnectServers = yaml.load(await fs.readFile(`${baseDir}/src/common/konnect.yaml`));
+  const s = konnectServers.servers.map((s) => {
+    s.url = s.url + path;
+    return s;
+  });
+
+  const geoServers = s.filter((s) => !s.url.includes("global.api.konghq.com"));
+  doc.servers = geoServers;
+
+  return doc;
+}
+
 if (require.main === module) {
   main();
 }
+
