db09d710c2831d3781e764b28ce966f24c7c58ab
Michael Heap <m@michaelheap.com>
2024-12-05 18:38:35 +0000

    Update all custom code to use terraform-provider-konnect/v2

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	custom "github.com/kong/terraform-provider-konnect/src"
+	custom "github.com/kong/terraform-provider-konnect/v2/src"
 	"github.com/kong/terraform-provider-konnect/v2/internal/sdk"
 	"github.com/kong/terraform-provider-konnect/v2/internal/sdk/models/shared"
 	"net/http"
