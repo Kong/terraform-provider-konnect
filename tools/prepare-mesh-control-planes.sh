ea802a69e2805ce3160af5c6ec1b9241902a826a
slonka <slonka@users.noreply.github.com>
2025-01-21 12:40:05 +0100

    feat(mink): get the oapi schema to pass validation in konnect-terraform-provider
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- tools/prepare-mesh-control-planes.sh
+++ tools/prepare-mesh-control-planes.sh
@@ -2,15 +2,14 @@
 set -e
 shopt -s globstar
 
-node ./tools/add-prefix-to-paths.js src/konnect/definitions/mesh-control-planes/v0/kong-mesh.yaml src/konnect/definitions/mesh-control-planes/v0/kong-mesh-prefixed.yaml "/mesh/control-planes/{cpId}" cpId "name: cpId
+node ./tools/add-prefix-to-paths.js src/konnect/definitions/mesh-control-planes/v0/kong-mesh.yaml src/konnect/definitions/mesh-control-planes/v0/kong-mesh-prefixed.yaml "/mesh/control-planes/{id}" cpId "name: id
 in: path
 required: true
 description: Id of the Konnect resource
 example: bf138ba2-c9b1-4229-b268-04d9d8a6410b
 schema:
   type: string
-  format: uuid # validation happens via KinConfig()
-x-speakeasy-match: id"
+  format: uuid # validation happens via KinConfig()"
 
 node ./tools/remove-common-definitions.js src/konnect/definitions/mesh-control-planes/v0/kong-mesh-prefixed.yaml src/konnect/definitions/mesh-control-planes/v0/kong-mesh-deduped.yaml
 
