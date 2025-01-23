253318379b0f379be3b16a1117fe6791ac47ae6f
Michael Heap <m@michaelheap.com>
2025-01-23 19:30:10 +0000

    Update test manifest token expiry date

--- tests/e2e/system-accounts.tf
+++ tests/e2e/system-accounts.tf
@@ -6,7 +6,7 @@ resource "konnect_system_account" "my_systemaccount" {
 
 resource "konnect_system_account_access_token" "my_systemaccountaccesstoken" {
   name       = "TF Managed Token"
-  expires_at = "2024-12-31T23:59:59Z"
+  expires_at = "2025-12-31T23:59:59Z"
   account_id = konnect_system_account.my_systemaccount.id
 }
 
