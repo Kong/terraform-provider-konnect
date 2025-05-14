76cbb7cd922878e433155f1fcc8b8b53bda746a9
slonka <slonka@users.noreply.github.com>
2025-05-14 10:44:53 +0200

    chore(release): kick off release 2.7.4
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- internal/sdk/konnect.go
+++ internal/sdk/konnect.go
@@ -296,9 +296,9 @@ func New(opts ...SDKOption) *Konnect {
 		sdkConfiguration: sdkConfiguration{
 			Language:          "go",
 			OpenAPIDocVersion: "2.0.0",
-			SDKVersion:        "2.7.3",
+			SDKVersion:        "2.7.4",
 			GenVersion:        "2.595.4",
-			UserAgent:         "speakeasy-sdk/terraform 2.7.3 2.595.4 2.0.0 github.com/kong/terraform-provider-konnect/v2/internal/sdk",
+			UserAgent:         "speakeasy-sdk/terraform 2.7.4 2.595.4 2.0.0 github.com/kong/terraform-provider-konnect/v2/internal/sdk",
 			Hooks:             hooks.New(),
 		},
 	}
