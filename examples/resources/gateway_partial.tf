579778cec7134920a7e75d0a728113f9c75e329c
Harsha Dixit <harsha.dixit@konghq.com>
2025-05-19 13:48:29 +0530

    Add an example for partial

---
+++ examples/resources/gateway_partial.tf
@@ -0,0 +1,21 @@
+resource "konnect_gateway_partial" "my_gatewaypartialce" {
+  control_plane_id = konnect_gateway_control_plane.tfdemo2.id
+  redis_ce = {
+    config = {
+      database    = 5
+      host        = "localhost"
+      password    = "rediscepass"
+      port        = 6379
+      server_name = "redisceservername"
+      ssl         = true
+      ssl_verify  = true
+      timeout     = 1000
+      username    = "redisceusername"
+    }
+    name       = "redic-ce-partial"
+    tags = [
+      "redisce"
+    ]
+    type       = "redis-ce"
+  }
+}
