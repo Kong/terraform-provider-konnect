052b66bbfc40a627999de18c1773150c863baaf3
Michael Heap <m@michaelheap.com>
2025-06-26 06:21:38 +0100

    Add missing custom validator

--- internal/validators/stringvalidators/centralized_consumer_key_validator.go
+++ internal/validators/stringvalidators/centralized_consumer_key_validator.go
@@ -12,7 +12,7 @@ type StringCentralizedConsumerKeyValidatorValidator struct{}
 
 // Description describes the validation in plain text formatting.
 func (v StringCentralizedConsumerKeyValidatorValidator) Description(_ context.Context) string {
-	return "TODO: add validator description"
+	return "Validates that type == 'legacy' if the secret field is provided"
 }
 
 // MarkdownDescription describes the validation in Markdown formatting.
@@ -22,11 +22,40 @@ func (v StringCentralizedConsumerKeyValidatorValidator) MarkdownDescription(ctx 
 
 // Validate performs the validation.
 func (v StringCentralizedConsumerKeyValidatorValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
-	resp.Diagnostics.AddAttributeError(
-		req.Path,
-		"TODO: implement stringvalidator CentralizedConsumerKeyValidator logic",
-		req.Path.String()+": "+v.Description(ctx),
-	)
+
+	// If attribute configuration is unknown, delay the validation until it is known.
+	if req.ConfigValue.IsUnknown() {
+		return
+	}
+
+	var typeValue string
+	if err := req.Config.GetAttribute(ctx, req.Path.ParentPath().AtName("type"), &typeValue); err != nil {
+		resp.Diagnostics.AddAttributeError(
+			req.Path,
+			"Invalid Configuration",
+			"Failed to retrieve the 'type' attribute.",
+		)
+		return
+	}
+
+	secretValue := req.ConfigValue.ValueString()
+	if secretValue != "" && typeValue != "legacy" {
+		resp.Diagnostics.AddAttributeError(
+			req.Path,
+			"Invalid Configuration",
+			"The 'secret' field can only be set if 'type' is set to 'legacy'.",
+		)
+		return
+	}
+
+	if secretValue == "" && typeValue == "legacy" {
+		resp.Diagnostics.AddAttributeError(
+			req.Path,
+			"Invalid Configuration",
+			"The 'secret' field must be provided if 'type' is set to 'legacy'.",
+		)
+		return
+	}
 }
 
 func CentralizedConsumerKeyValidator() validator.String {
