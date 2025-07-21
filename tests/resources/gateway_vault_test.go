6bdf4a53b1a309e864bb7cb8a71bcec0cd6debbf
Harsha Dixit <harsha.dixit@konghq.com>
2025-07-21 20:16:12 +0530

    add acceptance tests for vault, consumer, keyauth

---
+++ tests/resources/gateway_vault_test.go
@@ -0,0 +1,34 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestVault(t *testing.T) {
+	t.Run("update-nullify-fields", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			ProtoV6ProviderFactories: providerFactory,
+			Steps: []resource.TestStep{
+				{
+					Config:          providerConfigUs,
+					ConfigDirectory: config.TestNameDirectory(),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("konnect_gateway_vault.my_vault", "name", "env"),
+						resource.TestCheckResourceAttr("konnect_gateway_vault.my_vault", "description", "my vault description"),
+					),
+				},
+				{
+					// Update some fields to null
+					Config:          providerConfigUs,
+					ConfigDirectory: config.TestStepDirectory(),
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckNoResourceAttr("konnect_gateway_vault.my_vault", "description"),
+					),
+				},
+			},
+		})
+	})
+}
