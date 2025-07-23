04348c3f69d7c9c2e46aae74ce360e132c5a8fc0
Michael Heap <m@michaelheap.com>
2025-07-23 14:20:16 +0100

    Add examples

---
+++ examples/resources/catalog_service.tf
@@ -0,0 +1,27 @@
+resource "konnect_catalog_service" "my_catalogservice" {
+  name         = "tf-service"
+  display_name = "Terraform Service"
+  description  = "Hello World"
+  custom_fields = jsonencode({
+    "cost_center" : "eng",
+    "owner" : "MEE",
+    "product_manager" : "Your Name",
+    "dashboard" : {
+      "name" : "danny dashboard",
+      "link" : "https://app.example.com/dashboard/123",
+    },
+    "git_repo" : {
+      "name" : "example-repo",
+      "link" : "https://github.com/example/repo",
+    },
+    "jira_project" : null,
+    "slack_channel" : {
+      "name" : "test-channel",
+      "link" : "https://example.slack.com/archives/C098WKDB020"
+    },
+  })
+
+  labels = {
+    key = "value"
+  }
+}
