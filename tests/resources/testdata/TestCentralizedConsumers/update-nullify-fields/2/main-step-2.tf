e4feba5dc5a9480751fbbe85868fbb68d2456f1c
Harsha Dixit <harsha.dixit@konghq.com>
2025-07-24 12:09:50 +0530

    tests for api products

---
+++ tests/resources/testdata/TestCentralizedConsumers/update-nullify-fields/2/main-step-2.tf
@@ -0,0 +1,19 @@
+resource "konnect_gateway_control_plane" "centralized_consumer_cp" {
+  name         = "Centralized Consumers Control Plane for null support"
+  description  = "This is a sample description"
+  cluster_type = "CLUSTER_TYPE_CONTROL_PLANE"
+}
+
+resource "konnect_realm" "my_realm" {
+  name                     = "nullify-consumer-id"
+  ttl                      = 60
+  negative_ttl             = 10
+  allow_all_control_planes = false
+  allowed_control_planes   = [konnect_gateway_control_plane.centralized_consumer_cp.id]
+  force_destroy = "true"
+}
+
+resource "konnect_centralized_consumer" "my_centralizedconsumer" {
+  username  = "alice"
+  realm_id  = konnect_realm.my_realm.id
+}
