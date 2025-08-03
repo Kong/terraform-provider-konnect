1436d3dce91bc3bd5a21d6d479be29d14fee6a3b
Harsha Dixit <harsha.dixit@konghq.com>
2025-08-03 14:51:32 +0530

    tests: add tests for popular plugins

---
+++ tests/resources/gateway_plugin_request_transformer_test.go
@@ -0,0 +1,34 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestGatewayPluginRequestTransformer(t *testing.T) {
+	t.Parallel()
+	t.Run("update-nullify-fields", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			ProtoV6ProviderFactories: providerFactory,
+			Steps: []resource.TestStep{
+				{
+					Config:          providerConfigUs,
+					ConfigDirectory: config.TestNameDirectory(),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("konnect_gateway_plugin_jwt.my_jwt", "enabled", "true"),
+					),
+				},
+				{
+					// Update some fields to null
+					Config:          providerConfigUs,
+					ConfigDirectory: config.TestStepDirectory(),
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckNoResourceAttr("konnect_gateway_plugin_jwt.my_jwt", "config.anonymous"),
+					),
+				},
+			},
+		})
+	})
+}
---
+++ tests/resources/testdata/TestGatewayPluginRateLimitingAdvanced/update-nullify-fields/main.tf
@@ -0,0 +1,19 @@
+resource "konnect_gateway_control_plane" "plugin_ratelimiting_advanced_cp" {
+  name         = "Terraform Control Plane For RateLimitingAdvanced Plugin"
+  description  = "This is a sample description"
+  cluster_type = "CLUSTER_TYPE_CONTROL_PLANE"
+}
+
+resource "konnect_gateway_plugin_rate_limiting_advanced" "my_rate_limiting_advanced" {
+  enabled = true
+
+  config = {
+    limit = [200]
+    window_size = [1800]
+    window_type = "fixed"
+    namespace = "my-namespace"
+    header_name = "X-RateLimit-Limit"
+  }
+
+  control_plane_id = konnect_gateway_control_plane.plugin_ratelimiting_advanced_cp.id
+}
