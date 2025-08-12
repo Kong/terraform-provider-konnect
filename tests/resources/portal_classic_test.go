de92917edaee80ef47e74ce09d2919a39ebd1df0
Harsha Dixit <harsha.dixit@konghq.com>
2025-08-12 22:29:16 +0530

    tests: add acceptance tests for portal and portal_classic

--- tests/resources/portal_classic_test.go
+++ tests/resources/portal_classic_test.go
@@ -8,8 +8,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 )
 
-func TestPortal(t *testing.T) {
-	t.Run("appearance", func(t *testing.T) {
+func TestPortalClassic(t *testing.T) {
+	t.Run("plan-diff", func(t *testing.T) {
 		resource.Test(t, resource.TestCase{
 			ProtoV6ProviderFactories: providerFactory,
 			Steps: []resource.TestStep{
@@ -17,7 +17,7 @@ func TestPortal(t *testing.T) {
 					Config:          providerConfigUs,
 					ConfigDirectory: config.TestNameDirectory(),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttrSet("konnect_portal_appearance.test", "portal_id"),
+						resource.TestCheckResourceAttr("konnect_portal_classic.my_portal", "name", "My v2 Developer Portal"),
 					),
 				},
 				{
