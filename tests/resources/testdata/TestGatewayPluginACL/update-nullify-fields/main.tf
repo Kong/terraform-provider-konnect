82402d2bbbe8c6d7d64799c1d953dab7c42ff137
Harsha Dixit <harsha.dixit@konghq.com>
2025-08-12 14:29:29 +0530

    tests: fix misconfigured tests

--- tests/resources/testdata/TestGatewayPluginACL/update-nullify-fields/main.tf
+++ tests/resources/testdata/TestGatewayPluginACL/update-nullify-fields/main.tf
@@ -10,6 +10,7 @@ resource "konnect_gateway_plugin_acl" "my_acl" {
   config = {
     include_consumer_groups = true
     allow = ["dev", "admin"]
+    deny = ["test"]
   }
 
   control_plane_id = konnect_gateway_control_plane.plugin_acl_cp.id
