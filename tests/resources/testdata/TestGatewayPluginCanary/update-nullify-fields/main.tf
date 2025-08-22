b682e71333734476afedb1405d31c0b44b110ef1
Harsha Dixit <harsha.dixit@konghq.com>
2025-08-22 14:49:23 +0530

    tests: add CRUD test for canary plugin

---
+++ tests/resources/testdata/TestGatewayPluginCanary/update-nullify-fields/main.tf
@@ -0,0 +1,20 @@
+resource "konnect_gateway_control_plane" "plugin_canary_cp" {
+  name         = "Terraform Control Plane For Canary Plugin"
+  description  = "This is a sample description"
+  cluster_type = "CLUSTER_TYPE_CONTROL_PLANE"
+}
+
+resource "konnect_gateway_plugin_canary" "my_canary" {
+  enabled = true
+
+  config = {
+    duration = 3600
+    canary_by_header_name = "X-Canary-User"
+    upstream_host = "my_host"
+    upstream_port = 80
+    upstream_uri  = "test"
+    hash = "allow"
+  }
+
+  control_plane_id = konnect_gateway_control_plane.plugin_canary_cp.id
+}
