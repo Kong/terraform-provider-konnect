9475d09db448fe3fdace76ed77b52a7626452f8c
Harsha Dixit <harsha.dixit@konghq.com>
2025-08-22 10:55:16 +0530

    tests: fix for API implementation

--- tests/resources/api_implementation_test.go
+++ tests/resources/api_implementation_test.go
@@ -5,11 +5,10 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-testing/config"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 )
 
 func TestAPIImplementation(t *testing.T) {
-	t.Run("requires-replacement", func(t *testing.T) {
+	t.Run("CRUD", func(t *testing.T) {
 		resource.Test(t, resource.TestCase{
 			ProtoV6ProviderFactories: providerFactory,
 			Steps: []resource.TestStep{
@@ -19,16 +18,6 @@ func TestAPIImplementation(t *testing.T) {
 					Check: resource.ComposeAggregateTestCheckFunc(
 						resource.TestCheckResourceAttrSet("konnect_api_implementation.my_apiimplementation", "api_id"),
 					),
-				},
-				{
-					// Update service reference and verify it needs replacement
-					Config:          providerConfigUs,
-					ConfigDirectory: config.TestStepDirectory(),
-					ConfigPlanChecks: resource.ConfigPlanChecks{
-						PreApply: []plancheck.PlanCheck{
-							plancheck.ExpectResourceAction("konnect_api_implementation.my_apiimplementation", plancheck.ResourceActionDestroyBeforeCreate),
-						},
-					},
 				},
 			},
 		})
