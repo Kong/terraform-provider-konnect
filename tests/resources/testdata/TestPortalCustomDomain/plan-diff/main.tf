df912c867d9d5ca353c31eee7e69770ee8f51a4a
Harsha Dixit <harsha.dixit@konghq.com>
2025-10-13 09:14:13 +0530

    tests: update custom domain to avoid clash

--- tests/resources/testdata/TestPortalCustomDomain/plan-diff/main.tf
+++ tests/resources/testdata/TestPortalCustomDomain/plan-diff/main.tf
@@ -5,7 +5,7 @@ resource "konnect_portal" "my_portal_for_custom_domain" {
 
 resource "konnect_portal_custom_domain" "my_portal_custom_domain" {
   enabled   = false
-  hostname  = "my.custom.domain"
+  hostname  = "my.tftestingorgcustom.domain"
   portal_id = konnect_portal.my_portal_for_custom_domain.id
   ssl = {
     domain_verification_method = "http"
