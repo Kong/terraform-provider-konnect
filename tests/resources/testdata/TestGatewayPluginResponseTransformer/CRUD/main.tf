e9e03cf60e6b2872807146ea59f2f3f035a7922a
Harsha Dixit <harsha.dixit@konghq.com>
2025-10-21 10:14:30 +0530

    tests: request callout and response transformer plugins

---
+++ tests/resources/testdata/TestGatewayPluginResponseTransformer/CRUD/main.tf
@@ -0,0 +1,17 @@
+resource "konnect_gateway_control_plane" "plugincp" {
+  name         = "Terraform Control Plane For Response transformer Plugin"
+  description  = "This is a sample description"
+  cluster_type = "CLUSTER_TYPE_CONTROL_PLANE"
+}
+
+resource "konnect_gateway_plugin_response_transformer" "my_response_transformer" {
+  enabled = true
+
+  config = {
+    add = {
+      headers = ["New-Header:Header Value"]
+    }
+  }
+
+  control_plane_id = konnect_gateway_control_plane.plugincp.id
+}
