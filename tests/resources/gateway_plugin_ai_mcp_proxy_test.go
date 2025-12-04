f2ab068a60afbff21b7b947d7011a2841f2e3296
Harsha Dixit <harsha.dixit@konghq.com>
2025-12-04 16:00:40 +0530

    tests: add tests for ai-mcp-proxy request_body

--- tests/resources/gateway_plugin_ai_mcp_proxy_test.go
+++ tests/resources/gateway_plugin_ai_mcp_proxy_test.go
@@ -10,7 +10,7 @@ import (
 func TestGatewayPluginAiMcpProxy(t *testing.T) {
 	t.Parallel()
 
-	t.Run("smoke", func(t *testing.T) {
+	t.Run("CRUD", func(t *testing.T) {
 		resource.Test(t, resource.TestCase{
 			ProtoV6ProviderFactories: providerFactory,
 			Steps: []resource.TestStep{
@@ -19,6 +19,17 @@ func TestGatewayPluginAiMcpProxy(t *testing.T) {
 					ConfigDirectory: config.TestNameDirectory(),
 					Check: resource.ComposeAggregateTestCheckFunc(
 						resource.TestCheckResourceAttr("konnect_gateway_plugin_ai_mcp_proxy.my_plugin", "instance_name", "my-test-plugin"),
+						resource.TestCheckResourceAttr("konnect_gateway_plugin_ai_mcp_proxy.my_plugin", "config.tools.0.parameters.0.name", "id"),
+						resource.TestCheckResourceAttrSet("konnect_gateway_plugin_ai_mcp_proxy.my_plugin", "config.tools.1.request_body.content"),
+					),
+				},
+				{
+					Config:          providerConfigUs,
+					ConfigDirectory: config.TestStepDirectory(),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("konnect_gateway_plugin_ai_mcp_proxy.my_plugin", "config.max_request_body_size", "4096"),
+						resource.TestCheckNoResourceAttr("konnect_gateway_plugin_ai_mcp_proxy.my_plugin", "config.tools.0.parameters"),
+						resource.TestCheckNoResourceAttr("konnect_gateway_plugin_ai_mcp_proxy.my_plugin", "config.tools.1.request_body"),
 					),
 				},
 			},
---
+++ tests/resources/testdata/TestGatewayPluginAiMcpProxy/CRUD/2/main-step-2.tf
@@ -0,0 +1,38 @@
+resource "konnect_gateway_control_plane" "my_konnect_cp" {
+  name         = "Terraform Control Plane For AI MCP Proxy Plugin"
+  description  = "This is a sample description"
+  cluster_type = "CLUSTER_TYPE_CONTROL_PLANE"
+}
+
+resource "konnect_gateway_plugin_ai_mcp_proxy" "my_plugin" {
+  enabled       = true
+  instance_name = "my-test-plugin"
+
+  config = {
+    mode = "conversion-listener"
+    tools = [
+      {
+        description = "Get users"
+        method      = "GET"
+        path        = "/marketplace/users"
+        annotations = {
+          title = "Get users"
+        }
+      },
+      {
+        description = "Get orders for a user"
+        method      = "GET"
+        path        = "/marketplace/orders"
+        annotations = {
+          title = "Get users orders"
+        }
+    }]
+
+    server = {
+      timeout = 60000
+    }
+    max_request_body_size = 4096
+  }
+
+  control_plane_id = konnect_gateway_control_plane.my_konnect_cp.id
+}
