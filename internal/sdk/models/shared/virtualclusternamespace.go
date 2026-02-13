680acb492cbeb4e1a31b9082e37995f0094ccfb0
Harsha Dixit <harsha.dixit@konghq.com>
2026-02-13 11:33:12 +0530

    chore: prepare to release 3.7.0
    Signed-off-by: Harsha Dixit <harsha.dixit@konghq.com>

--- internal/sdk/models/shared/virtualclusternamespace.go
+++ internal/sdk/models/shared/virtualclusternamespace.go
@@ -9,9 +9,9 @@ import (
 
 // Mode - * hide_prefix - the configured prefix is hidden from clients for topics and IDs when reading.
 //
-//		Created resources are written with the prefix on the backend cluster.
-//	  - enforce_prefix - the configured prefix remains visible to clients.
-//	    Created resources must include the prefix or the request will fail.
+//	Created resources are written with the prefix on the backend cluster.
+//   - enforce_prefix - the configured prefix remains visible to clients.
+//     Created resources must include the prefix or the request will fail.
 type Mode string
 
 const (
