8f0bcf86233719f9c992f36bb8899fafa7599db4
Harsha Dixit <harsha.dixit@konghq.com>
2026-02-17 18:48:06 +0530

    tests: event gateway listener policy with sni, and app auth strategy

---
+++ tests/resources/application_auth_strategy_test.go
@@ -0,0 +1,80 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Kong/shared-speakeasy/hclbuilder"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	applicationAuthStrategy = `
+		resource "konnect_application_auth_strategy" "my_applicationauthstrategy" {
+		  key_auth = {
+		    name          = "my-application-auth-strategy"
+		    display_name  = "My Test Strategy"
+		    strategy_type = "key_auth"
+		    configs = {
+		      key_auth = {
+		        key_names = ["apikey"]
+		      }
+		    }
+		  }
+		}
+	`
+)
+
+func TestApplicationAuthStrategy(t *testing.T) {
+	serverHost, serverPort, serverScheme := providerConfigFromEnv()
+	providerConfigTemplate := "%s://%s:%d"
+
+	t.Run("Application Auth Strategy", func(t *testing.T) {
+		builder := hclbuilder.NewWithProvider(hclbuilder.Konnect, fmt.Sprintf(providerConfigTemplate, serverScheme, serverHost, serverPort))
+		builder.ProviderProperty = hclbuilder.Konnect
+
+		authStrategy, err := hclbuilder.FromString(applicationAuthStrategy)
+		require.NoError(t, err)
+
+		resource.Test(t, resource.TestCase{
+			ProtoV6ProviderFactories: providerFactory,
+			Steps: []resource.TestStep{
+				{
+					Config: builder.Upsert(authStrategy).Build(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectResourceAction("konnect_application_auth_strategy.my_applicationauthstrategy", plancheck.ResourceActionCreate),
+						},
+					},
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("konnect_application_auth_strategy.my_applicationauthstrategy", "key_auth.name", "my-application-auth-strategy"),
+						resource.TestCheckResourceAttr("konnect_application_auth_strategy.my_applicationauthstrategy", "key_auth.display_name", "My Test Strategy"),
+						resource.TestCheckResourceAttr("konnect_application_auth_strategy.my_applicationauthstrategy", "key_auth.strategy_type", "key_auth"),
+						resource.TestCheckResourceAttr("konnect_application_auth_strategy.my_applicationauthstrategy", "key_auth.configs.key_auth.key_names.0", "apikey"),
+					),
+				},
+				{
+					Config: builder.Upsert(authStrategy).Build(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectEmptyPlan(),
+						},
+					},
+				},
+				{
+					Config: builder.Upsert(authStrategy.AddAttribute("key_auth.display_name", `"Updated Test Strategy"`)).Build(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectResourceAction("konnect_application_auth_strategy.my_applicationauthstrategy", plancheck.ResourceActionUpdate),
+						},
+					},
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("konnect_application_auth_strategy.my_applicationauthstrategy", "key_auth.display_name", "Updated Test Strategy"),
+					),
+				},
+			},
+		})
+	})
+}
