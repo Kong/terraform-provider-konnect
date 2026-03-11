16cb4a85de13dfd5b04c7e04e551fd91a2ff5e97
Harsha Dixit <harsha.dixit@konghq.com>
2026-03-11 12:07:07 +0530

    example: add example for cgw add on, and scenario for event gateway policy ordering

---
+++ examples/resources/cloud_gateway_addon.tf
@@ -0,0 +1,25 @@
+resource "konnect_gateway_control_plane" "test_cp" {
+    name         = "CGW Control Plane"
+    cloud_gateway = true
+}
+
+resource "konnect_cloud_gateway_addon" "my_addon" {
+    name     = "My Add-on"
+
+    config = {
+        managed_cache = {
+            capacity_config = {
+                tiered = {
+                    tier = "small"
+                }
+            }
+        }
+    }
+    owner = {
+        control_plane = {
+            kind             = "control-plane"
+            control_plane_geo = "us"
+            control_plane_id  = konnect_gateway_control_plane.test_cp.id
+        }
+    }
+}
