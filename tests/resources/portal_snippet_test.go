60a5123a934ae2179fbd35a196bc6b49371f1fe6
shivaygupta <shivay.gupta@konghq.com>
2026-07-06 14:19:03 +0530

    Test/portal snippet update test (#458)
    * test: added tests for konnect_portal_snippet
    
    * chore: added changelog.
    
    * chore: removed changelog

--- tests/resources/portal_snippet_test.go
+++ tests/resources/portal_snippet_test.go
@@ -32,4 +32,27 @@ func TestPortalSnippet(t *testing.T) {
 			},
 		})
 	})
+
+	t.Run("update", func(t *testing.T) {
+		resource.Test(t, resource.TestCase{
+			ProtoV6ProviderFactories: providerFactory,
+			Steps: []resource.TestStep{
+				{
+					Config:          providerConfigUs,
+					ConfigDirectory: config.StaticDirectory("testdata/TestPortalSnippet/plan-diff"),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("konnect_portal_snippet.my_portal_snippet", "name", "my-snippet"),
+					),
+				},
+				{
+					Config:          providerConfigUs,
+					ConfigDirectory: config.TestNameDirectory(),
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("konnect_portal_snippet.my_portal_snippet", "content", "# Updated Content"),
+						resource.TestCheckResourceAttr("konnect_portal_snippet.my_portal_snippet", "description", "Updated description"),
+					),
+				},
+			},
+		})
+	})
 }
---
+++ tests/resources/testdata/TestPortalSnippet/update/main.tf
@@ -0,0 +1,13 @@
+resource "konnect_portal" "my_portal_for_snippet" {
+  force_destroy = "true"
+  name         = "My v3 portal name"
+}
+
+resource "konnect_portal_snippet" "my_portal_snippet" {
+  content     = "# Updated Content"
+  description = "Updated description"
+  name        = "my-snippet"
+  title       = "My Snippet"
+  portal_id   = konnect_portal.my_portal_for_snippet.id
+}
+
