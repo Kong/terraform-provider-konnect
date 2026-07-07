b203f9ed36c67651a6de1a8032ffef867b60045c
Varun Athreya <varun.athreya@konghq.com>
2026-07-07 14:16:09 +0530

    feat: add example and acceptance test

---
+++ tests/resources/testdata/TestOrgPATSettings/CRUD/main.tf
@@ -0,0 +1,5 @@
+resource "konnect_organization_personal_access_token_settings" "my_organizationpersonalaccesstokensettings" {
+  max_expiration_period_days = 364
+  organization_id            = "72ab1dcd-3e4a-4f9b-9e9d-94217898055a"
+  pats_enabled               = true
+}
