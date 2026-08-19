4d06f0f1e6088bf8a37a8635b8c09183b868a822
shivay <shivay.gupta@konghq.com>
2026-08-19 15:32:52 +0530

    tests: updated images for portal-favicon and portal-logo tests.

--- tests/resources/testdata/TestPortalFavicon/plan-diff/main.tf
+++ tests/resources/testdata/TestPortalFavicon/plan-diff/main.tf
@@ -4,6 +4,6 @@ resource "konnect_portal" "my_portal_for_favicon" {
 }
 
 resource "konnect_portal_favicon" "my_portal_favicon" {
-  data      = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=="
+  data      = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAEElEQVR4nGL6n50NCAAA//8ESgHYqxSkJQAAAABJRU5ErkJggg=="
   portal_id = konnect_portal.my_portal_for_favicon.id
 }
