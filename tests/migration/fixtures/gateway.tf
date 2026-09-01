91fa3248e9a58ab0066d4e7653ad8486ea6554ff
Varun Athreya <varun.athreya@konghq.com>
2026-09-01 16:40:12 +0530

    feat: update migration key resource to use PEM format for private and public keys

--- tests/migration/fixtures/gateway.tf
+++ tests/migration/fixtures/gateway.tf
@@ -360,14 +360,34 @@ resource "konnect_gateway_key_set" "migration_key_set" {
 resource "konnect_gateway_key" "migration_key" {
   kid  = "migration-kid-1"
   name = "migration-key"
-  jwk = jsonencode({
-    kty = "RSA"
-    kid = "migration-kid-1"
-    use = "sig"
-    alg = "RS256"
-    e   = "AQAB"
-    n   = "tk5VuZUPrDvdDgS_Fouj2q1PXWyuYC0vEdKE6uZVrUrsoJlBAQZXNiMt3YtBvBUjXOEtRCXn7giRbHfqPxTFvNwKsiCIJZC7rsN6OCeQgC2hbiE77yAQtlWk_mWwvcVrg1Xo-ZcoxG592iJo9dTKRqpVmEWePNqIbUXJH4s3jjyPdkTqgfY9NZrBezilnAGgRlvDKJIye6iOqpw4KFqYaWcoiBTfgnTYhKGbhPrPRDFhspEfu6deTx069i84exMcExiaGRM-Bco7WfemnstkwGfi4u5jrVCQBg7r7TaBv37I4DY7uqOoWv1swobhM2Jk3vGRoLEZTiiIjPkTb2ZH6Q"
-  })
+
+  pem              = {
+    private_key = <<-EOT
+          -----BEGIN RSA PRIVATE KEY-----
+          MIICXAIBAAKBgG79jfVbRQHMEV/qaskL6WVeClxYD17q3hWBr6xLpVENedhky8ps
+          dXVoqLkbWMW+uYHB9vYgXb9yI+Mu9EYQ4y6lmi18nPX3qkYsNHCkItcjAnVSHkeC
+          h+S7lsHvnTALu/0y/emldJiWqh38wKnlZxE+2QcdELKTomExyeuI6Xp9AgMBAAEC
+          gYACCm1yxnPVXaAwKEpAWhS3hWwhWIkn0J+2u4S0YUuv2tSTsv7AQRBD0WHQzXzr
+          Xd9hUGSvD9dJbtrUEYfyRds3eR6j5QOUd9ITgya4Bsyk9G2YLoSa0xRRIO1EG4gw
+          WU8LsePrvjtBDvN1TipmCy0LpS4W2A26D/AzGnIdVi5SbQJBALr0q+dV4xGnLU8N
+          6DHeU+fwf9sc5SYkOQ0xoPBW8Tp2oaYRTguyCqBrDUc5TySxHFTR1hW8a7FVSwE2
+          xgNmDdMCQQCX+uhE3xWTQQ+uJn7GrfBp541XZV/g7Ur3Lk9akGpDe/S9O0qgIk6b
+          2JxeECBrgAhpV0eEjWacIzm+caBA9xRvAkEAtw/VM5QZ37M+3mXTsuDsb/RCfZ7x
+          kkNtycmKuUuosYJwDlhrf3A6j5HDUrZ/FIKIJ1XAt0/kc6P86ZdklPddGQJAEu4S
+          rjlnM49AB74Nvmt97YME7OTYm2iTFNS015/zTHKNGmDfO7DqP6ksWN8DWsB4y74u
+          T0lZhYsxrxHyFwVviwJBAJFjgPNOdiiLGCZH+jTxn13AYeK+FRCrpLEQUzyNlcHB
+          DFgOK+hv6mppQlmApBsC82rTSsBjl1l7R1qHDFQcZR8=
+          -----END RSA PRIVATE KEY-----
+      EOT
+    public_key  = <<-EOT
+          -----BEGIN PUBLIC KEY-----
+          MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgG79jfVbRQHMEV/qaskL6WVeClxY
+          D17q3hWBr6xLpVENedhky8psdXVoqLkbWMW+uYHB9vYgXb9yI+Mu9EYQ4y6lmi18
+          nPX3qkYsNHCkItcjAnVSHkeCh+S7lsHvnTALu/0y/emldJiWqh38wKnlZxE+2Qcd
+          ELKTomExyeuI6Xp9AgMBAAE=
+          -----END PUBLIC KEY-----
+      EOT
+  }
 
   set = {
     id = konnect_gateway_key_set.migration_key_set.id
