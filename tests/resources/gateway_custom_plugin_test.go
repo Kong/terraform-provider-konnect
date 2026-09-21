61ec7fb2b1920c069f568496875a442da985231e
Harsha Dixit <harsha.dixit@konghq.com>
2026-09-21 13:40:31 +0530

    tests: update custom plugin test condition

--- tests/resources/gateway_custom_plugin_test.go
+++ tests/resources/gateway_custom_plugin_test.go
@@ -28,6 +28,8 @@ func TestGatewayCustomPlugin(t *testing.T) {
 						resource.TestCheckNoResourceAttr("konnect_gateway_custom_plugin.custom_no_instance_name", "instance_name"),
 						resource.TestCheckResourceAttr("konnect_gateway_custom_plugin.custom_basic_auth_with_ordering", "ordering.before.access.0", "acl"),
 						resource.TestCheckResourceAttr("konnect_gateway_custom_plugin.custom_basic_auth_with_ordering", "ordering.after.access.0", "request-transformer"),
+						resource.TestCheckResourceAttr("konnect_gateway_custom_plugin.custom_rate_limiting_with_partial", "partials.0.name", "custom-plugin-redis-ce-partial"),
+						resource.TestCheckResourceAttr("konnect_gateway_custom_plugin.custom_rate_limiting_with_partial", "condition", "http.method == \"GET\""),
 					),
 				},
 				{
